6bb88ff0ac9b7aa2e32c1f30b5ffd755cc74f705
sanjivsrao <sanjivr@vt.edu>
2023-02-19 13:38:54 -0500

    Update gyro_code.ino

--- gyro_code/gyro_code.ino
+++ gyro_code/gyro_code.ino
@@ -1,18 +1,15 @@
 #include <SoftwareSerial.h>
 #include <MPU6050_light.h>
 #include "Wire.h"
-#define rxPin 2
-#define txPin 3
+#define sdaPin 4
+#define sclPin 5
 
 unsigned long timer = 0;
 // Set up a new SoftwareSerial object
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+SoftwareSerial mySerial =  SoftwareSerial(sdaPin, sclPin);
 MPU6050 mpu(Wire);
 
 void setup()  {
-    // Define pin modes for TX and RX
-    pinMode(rxPin, INPUT);
-    pinMode(txPin, OUTPUT);
     mpu.begin();
     delay(1000);
     mpu.calcOffsets();
