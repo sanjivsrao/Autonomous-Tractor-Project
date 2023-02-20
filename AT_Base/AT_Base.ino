02fc79566c484944732c4ef66d420fa11c182797
sanjivsrao <sanjivr@vt.edu>
2023-02-19 23:18:39 -0500

    Working Code

--- AT_Base/AT_Base.ino
+++ AT_Base/AT_Base.ino
@@ -27,7 +27,7 @@ SoftwareSerial mySerial(8, 9);
 MPU6050 mpu(Wire);
 
 const unsigned long motorTimerPreset = 2000;  // two seconds
-unsigned long timerMillis;  // For counting time increments
+unsigned long timer = 0;
 
 int buttonState;
 bool switcher = false;
@@ -42,7 +42,7 @@ float z;
 float z_init;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   //button
@@ -61,19 +61,25 @@ void setup() {
   //init direction for right motor
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
+
+  
   //gyro setup
   Wire.begin();
   mpu.begin();
-  mpu.calcOffsets();
+  mpu.calcGyroOffsets();
   mySerial.begin(9600);
   z_init = mpu.getAngleZ();
-  Serial.begin(9600);
+
   //init state
   currentState = OFF;
 }
 
 void loop() {
   mpu.update();
+  float x = mpu.getAngleX();
+  float y = mpu.getAngleY();
+  float z = -(mpu.getAngleZ());
+  
   if (mySerial.available()) {
     cmd = mySerial.readString();
     if (cmd == "off") {
@@ -86,19 +92,16 @@ void loop() {
       mySerial.println("Invalid command");
     }
   }
-
-  buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  if (buttonState == 1) {
-    switcher = true;
-  }
-  delay (250);
+  button_check();
+  
   //debounce button
   switch (currentState) {
     case OFF: // Nothing happening, waiting for switchInput
+      delay (250);
+      button_check();
       analogWrite(enA, 0);
       analogWrite(enB, 0);
-      Serial.println("OFF");
+      mySerial.println("OFF");
       if (cmd == "on" || switcher) {
         switcher = false;
         currentState = MOVE;
@@ -110,34 +113,33 @@ void loop() {
       }
 
     case MOVE:
-      Serial.println("Moving!!!!!");
-      analogWrite(enA, 200);
-      analogWrite(enB, 200);
+      button_check();
       if (cmd == "off" || switcher) {
         switcher = false;
         currentState = OFF;
-        timerMillis = millis();
+        timer = millis();
+        break;
+      }       
+      mySerial.println("Moving!!!!!");
+      analogWrite(enA, 200);
+      analogWrite(enB, 200);                                                       
+      if (z > z_init) {
+        mySerial.println("turn left");
+        analogWrite(enA, 180);
+        analogWrite(enB, 130);
+        delay(500);
+        currentState = MOVE;
         break;
       }
-      z = mpu.getAngleZ();
-      Serial.println(z);
-      if ((millis() - timerMillis) > 1000) {
-        if (z > 0) {
-          analogWrite(enA, 100);
-          analogWrite(enB, 140);
-          currentState = MOVE;
-          delay(1000);
-          break;
-        }
-        if (z < 0) {
-          analogWrite(enA, 140);
-          analogWrite(enB, 100);
-          currentState = MOVE;
-          delay(1000);
-          break;
-        }
-        timerMillis = millis();
+      if (z < z_init) {
+        mySerial.println("turn right");
+        analogWrite(enA, 130);
+        analogWrite(enB, 180);
+        delay(500);
+        currentState = MOVE;
+        break;
       }
+      
       break;
 
       //      digitalWrite(openLED, motorRun);
@@ -174,4 +176,13 @@ void loop() {
   // Clears the command
   cmd = "";
   buttonState = 0;
+}
+
+void button_check(){
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  if (buttonState == 1) {
+    switcher = true;
+  }
+  delay (100);
 }
