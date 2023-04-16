81d62a10bcbcabc12b6c65afd0cac9ffeb8c8a96
sanjivsrao <sanjivr@vt.edu>
2023-04-15 21:05:03 -0400

    Update bleaktesting.py

--- python_gui/bleaktesting.py
+++ python_gui/bleaktesting.py
@@ -8,7 +8,10 @@ s_UIUD = "0000FFE0-0000-1000-8000-00805F9B34FB"
 
 async def main(address):
     async with BleakClient(address) as client:
-        await client.write_gatt_char(c_UUID, bytearray('off', 'utf8'))
+        await client.write_gatt_char(c_UUID, bytearray('off', 'unicode_escape'))
+        dataFromJawn = await client.read_gatt_char(c_UUID)
+        
+        print(dataFromJawn.decode()) 
         #model_number = await client.write_gatt_char(c_UUID)
         #print("Model Number: {0}".format("".join(map(chr, model_number))))
 
